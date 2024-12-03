da07e6eac1959d255de4aad0b6bf693a37b2338c
Dys <50160441+Dystopia-user181@users.noreply.github.com>
2024-12-03 13:30:49 +0800

    RSA demo

---
+++ rsa.js
@@ -0,0 +1,20 @@
+const p = 41, q = 67, n = p * q, l = (p-1)*(q-1), e = 7, d = 2263;
+
+function modExp(a, n, m) {
+	let ans = 1;
+		console.log(a, n)
+	while (n) {
+		if (n & 1) ans *= a; ans = ans % m;
+		a *= a; a = a % m;
+		n >>= 1;
+	}
+	return ans;
+}
+
+document.getElementById("m").addEventListener("input", xx => {
+	let m = Number(xx.target.value);
+	let mp = modExp(m, e, n);
+	document.getElementById("m'").innerHTML = `m' = m<sup>e</sup> = ${mp}`;
+	let mpp = modExp(mp, d, n);
+	document.getElementById("m''").innerHTML = `m'' = m'<sup>d</sup> = ${mpp}`;
+});
