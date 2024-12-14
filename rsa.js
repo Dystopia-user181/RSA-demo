6c855af4ce33f216efebb086830c174314f3bb61
Dys <50160441+Dystopia-user181@users.noreply.github.com>
2024-12-14 14:32:09 +0800

    Increase numbers to show power of RSA

--- rsa.js
+++ rsa.js
@@ -1,24 +1,23 @@
-const p = 41, q = 67, n = p * q, l = (p-1)*(q-1), e = 7, d = 2263;
+const p = 1282529n, q = 1524181n, n = p * q, e = 11n, d = 1777094115491n;
 
 function modExp(a, n, m) {
-	let ans = 1;
-		console.log(a, n)
+	let ans = 1n;
 	while (n) {
-		if (n & 1) ans *= a; ans = ans % m;
+		if (n & 1n) ans *= a; ans = ans % m;
 		a *= a; a = a % m;
-		n >>= 1;
+		n = n >> 1n
 	}
 	return ans;
 }
 
 document.getElementById("m").addEventListener("input", xx => {
-	let m = Number(xx.target.value);
+	let m = BigInt(xx.target.value);
 	let mp = modExp(m, e, n);
 	document.getElementById("m'").innerHTML = `m' = m<sup>e</sup> = ${mp}`;
 });
 
 document.getElementById("m'in").addEventListener("input", xx => {
-	let mp = Number(xx.target.value);
+	let mp = BigInt(xx.target.value);
 	let mpp = modExp(mp, d, n);
 	document.getElementById("m''").innerHTML = `m'' = m'<sup>d</sup> = ${mpp}`;
 });
